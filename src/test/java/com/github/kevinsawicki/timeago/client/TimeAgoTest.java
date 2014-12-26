303c46f63b39bb38fe5522c0ef364dc2d43eac8b
Kerby Martino <kerbymart@gmail.com>
2014-12-27 03:02:27 +0800

    make proper gwt test structure

--- src/test/java/com/github/kevinsawicki/timeago/client/TimeAgoTest.java
+++ src/test/java/com/github/kevinsawicki/timeago/client/TimeAgoTest.java
@@ -19,7 +19,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-package com.github.kevinsawicki.timeago.tests;
+package com.github.kevinsawicki.timeago.client;
 
 import com.github.kevinsawicki.timeago.client.TimeAgo;
 
