745cfc254c9300322e7575060785017aa6a95c7a
Kerby Martino <kerbymart@gmail.com>
2015-01-23 18:18:42 +0800

    fixing String formatter

--- src/main/java/com/github/kerbymart/timeago/client/TimeAgo.java
+++ src/main/java/com/github/kerbymart/timeago/client/TimeAgo.java
@@ -434,14 +434,16 @@ public class TimeAgo {
 		return this;
 	}
 
-	public static String format(final String format, final Object... args) {
-		final RegExp regex = RegExp.compile("%[a-z]");
+	public static String format(final String format, final Object arg) {
+		final RegExp regex = RegExp.compile("{(.*?)}");
 		final SplitResult split = regex.split(format);
 		final StringBuffer msg = new StringBuffer();
-		for (int pos = 0; pos < split.length() - 1; ++pos) {
-			msg.append(split.get(pos));
-			msg.append(args[pos].toString());
-		}
+//		for (int pos = 0; pos < split.length() - 1; ++pos) {
+//			msg.append(split.get(pos));
+//			msg.append(String.valueOf(arg));
+//		}
+		msg.append(split.get(0));
+		msg.append(String.valueOf(arg));
 		msg.append(split.get(split.length() - 1));
 		return msg.toString();
 	}
